6118aefec64325d1356bd010f1cfad878de03ce2
VincenzoIuliano <bubucane@outlook.it>
2024-11-27 15:27:02 +0100

    ADD: first css rules and html scafolding on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import tasks from "../tasks.js"
 
 function App() {
-  
+
+  // prepraro array in progress e backlog
+
+
+  // preparo array completed 
 
   return (
     <>
-      
+      <header className="main-header">
+        <div className="container">
+          <h1>Task Manager</h1>
+        </div>
+      </header>
+      <main>
+        <section>
+          <div className="container">
+            <h2 className="task-title"></h2>
+          </div>
+          <div className="container">
+            <ul className="task-list">
+              <li>task</li>
+            </ul>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
